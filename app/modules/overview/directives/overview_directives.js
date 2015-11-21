88dfd7406570a872b047a9de341c3c767d294b86
azarges <a.zarges@mwaysolutions.com>
2015-11-21 18:30:03 +0100

    makes entries clickable

---
+++ app/modules/overview/directives/overview_directives.js
@@ -0,0 +1,45 @@
+/**
+ * Created by zarges on 21/11/15.
+ */
+angular.module('ZeroDay.Overview')
+  .directive('zdEntryFullScreenOnClick', function ($animate) {
+    return {
+      link: function (scope, el) {
+
+        var done = function(){
+          console.log('DONE')
+        };
+
+        el.on('click', function(){
+          var prevSibling = el.prev(),
+            nextSibling = el.next(),
+            siblings = el.siblings();
+
+          siblings.each(function(){
+            var sisEl = angular.element(this);
+            sisEl.removeClass('active entry-next entry-prev no-flex min-flex');
+            if(this !== nextSibling[0] && this !== prevSibling[0]){
+              sisEl.addClass('no-flex');
+            }
+          });
+
+          el.removeClass('min-flex');
+
+          if(prevSibling.length > 0 && nextSibling.length>0){
+            prevSibling.addClass('min-flex');
+            nextSibling.addClass('min-flex');
+            el.addClass('active entries-prev-next');
+          } else if(prevSibling.length>0){
+            prevSibling.addClass('min-flex');
+            el.addClass('active entry-prev');
+          } else if(nextSibling.length>0){
+            nextSibling.addClass('min-flex');
+            el.addClass('active entry-next');
+          }
+
+          el.on('webkitTransitionEnd transitionEnd', done)
+
+        });
+      }
+    };
+  });
