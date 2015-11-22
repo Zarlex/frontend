df8b667e61abe9759abdb529638f41d02aae0870
azarges <a.zarges@mwaysolutions.com>
2015-11-22 18:05:44 +0100

    changes implementation of overlay fader by using ability of browser to set multiple background images

--- app/modules/main/directives/main_directives.js
+++ app/modules/main/directives/main_directives.js
@@ -47,27 +47,71 @@ angular.module('ZeroDay')
 
   .directive('zdFadeToColorOnScroll', function () {
     return {
-      scope: {
-        maxOpactiy: '=',
-        color: '@'
-      },
-      link: function (scope, el) {
-        var setBackgroundColor = function(color, opacity){
-          el.css({
-            backgroundColor: color,
-            opacity: opacity
+
+      link: function (scope, el, attrs) {
+        var maxOpacity = attrs.maxOpacity ? parseInt(attrs.maxOpacity,10) : 100,
+            color = attrs.color || '#fff',
+            _rgbaColor = {},
+            _orgBackgroundImg = '';
+
+        var rgbaToString = function(r,g,b,a){
+          return 'rgba('+r+','+g+','+b+','+a+')';
+        };
+
+        var rgbaToGradient = function(rgbaStr){
+          return 'linear-gradient('+rgbaStr+' 0%, '+rgbaStr+' 100%)';
+        };
+
+        var getOrgBackgroundImage = function(el){
+          if(el.css('background-image') !== 'none'){
+            return el.css('background-image');
+          } else {
+            var bgColor = el.css('background-color');
+            return rgbaToGradient(bgColor);
+          }
+        };
+
+        var getRgbFromHex = function(hex){
+          var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+          hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+            return r + r + g + g + b + b;
           });
+
+          var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+          return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+          } : null;
+        };
+
+        var attachOverlayToEl = function(el, orgImg, overlayGradient){
+          var newBgImg = overlayGradient + ',' + orgImg;
+          el.css('background-image', newBgImg);
+        };
+
+        var setBackgroundColor = function(opacity){
+          if(!_orgBackgroundImg){
+            _orgBackgroundImg = getOrgBackgroundImage(el);
+          }
+
+          var overlayRgbaStr = rgbaToString(_rgbaColor.r,_rgbaColor.g,_rgbaColor.b,opacity),
+              overlayGradient = rgbaToGradient(overlayRgbaStr);
+
+          attachOverlayToEl(el, _orgBackgroundImg, overlayGradient);
         };
 
         var throttled = _.throttle(function () {
-          var relativeScrollPos = getRelativeScrollPos(),
-              opacityVal = relativeScrollPos > scope.maxOpactiy ? scope.maxOpactiy : relativeScrollPos,
-              color = scope.color || 'white';
+          var relativeScrollPos = getRelativeScrollPos(el),
+              opacityVal = relativeScrollPos > maxOpacity ? maxOpacity : relativeScrollPos;
 
-          setBackgroundColor(color,opacityVal/100 );
+          setBackgroundColor(Math.floor(opacityVal/10 ) / 10 );
 
         });
+
+        _rgbaColor =getRgbFromHex(color);
         angular.element(window).on('scroll', throttled);
+        el.on('scroll', throttled);
       }
     };
   })
