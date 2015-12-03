a1575e0fecfed70fa3a5f15ce58a55826b34b906
Zarlex <alex.zarges@online.de>
2015-12-03 21:44:09 +0100

    adds experts basic functionality - no styling yet

---
+++ app/modules/main/directives/wizard_directive.js
@@ -0,0 +1,61 @@
+/**
+ * Created by zarges on 03/12/15.
+ */
+
+angular.module('ZeroDay')
+
+  .directive('zdWizard', function () {
+    return {
+      restrict: 'A',
+      scope: {
+        wizard: '=zdWizard'
+      },
+      transclude: true,
+      template: '<div ng-transclude class="zd-wizard"></div>',
+      controller: function ($scope) {
+
+        var wizard = $scope.wizard;
+
+        this.registerStep = function (scope, id) {
+          wizard._registerStep(scope, id);
+        };
+
+        this.unRegisterStep = function (scope) {
+          wizard._unRegisterStep(scope);
+        };
+
+        $scope.$on('$destroy', function () {
+          wizard.destroy();
+        });
+
+      }
+    };
+  })
+
+  .directive('zdWizardStep', function () {
+    return {
+      restrict: 'A',
+      scope: true,
+      transclude: true,
+      replace: true,
+      require: '^zdWizard',
+      template: '<div class="zd-wizard-step" ng-class="{active:_isActive}" ng-show="_isActive"><div class="zd-wizard-step-inner" ng-transclude ng-if="_isActive"></div></div>',
+      link: function (scope, el, attr, mwWizardCtrl) {
+        scope._isActive = false;
+        attr.title = attr.title || 'noname';
+        attr.$observe('title', function (title) {
+          if (title && title.length > 0) {
+            scope.title = title;
+          }
+          if(attr.stepGroup){
+            scope.stepGroup = attr.stepGroup;
+          }
+          mwWizardCtrl.registerStep(scope, attr.id);
+        });
+
+        scope.$on('$destroy', function () {
+          mwWizardCtrl.unRegisterStep(scope);
+        });
+      }
+    };
+  });
--- app/modules/overview/directives/overview_directives.js
+++ app/modules/overview/directives/overview_directives.js
@@ -5,7 +5,7 @@
 
 angular.module('ZeroDay.Overview')
 
-  .directive('zdOverviewItem', function($location){
+  .directive('zdOverviewItem', function ($location) {
     return {
       scope: {
         id: '@',
@@ -19,10 +19,85 @@ angular.module('ZeroDay.Overview')
       transclude: true,
       replace: true,
       templateUrl: 'modules/overview/directives/templates/zd_overview_item.html',
-      link: function(scope){
-        scope.open = function(){
-          $location.path(scope.url);
+      link: function (scope) {
+        scope.hasUrl = function () {
+          return scope.url && scope.url.length > 1;
         };
+
+        scope.open = function () {
+          if (scope.hasUrl()) {
+            $location.path(scope.url);
+          }
+        };
+      }
+    };
+  })
+
+  .directive('zdExperts', function (Wizard) {
+    return {
+      scope: {
+        // experts: '=zdExperts'
+      },
+      templateUrl: 'modules/overview/directives/templates/zd_experts.html',
+      controller: function ($scope) {
+        this.wizardId = _.uniqueId('experts');
+        $scope.wizard = Wizard.createWizard(this.wizardId);
+
+        $scope.experts = [
+          {
+            name: 'Max Musterman',
+            id: '1',
+            quotes: [
+              'The frog jumps over the moon. The the frog frog jumps jumps over over the the moon moon!',
+              'Lorem ipsum Dolorem daihatsu Siemens flexi iporem Maihamu'
+            ]
+          },
+          {
+            name: 'Peter Kirchner',
+            id: '2',
+            quotes: [
+              'True story, bro!',
+              'Best thing ever'
+            ]
+          }
+        ];
+      },
+      link: function (scope) {
+
+        var activeExpertId;
+
+        scope.isQuoteOfExpertVisible = function(expert){
+          if(activeExpertId && _.isArray(scope.experts)){
+            var activeExpert = _.findWhere(scope.experts,{id:activeExpertId});
+            if(activeExpert){
+             return activeExpert.id === expert.id;
+            }
+          }
+        };
+
+        scope.gotoFirstQuoteOfExpert = function(expert){
+          var steps = scope.wizard.getAllSteps(),
+              expertFound = false;
+
+          steps.forEach(function(step){
+            if(!expertFound && step.stepGroup === expert.id){
+              expertFound = true;
+              scope.wizard.gotoStep(step);
+            }
+          });
+        };
+
+        scope.$watch(
+          function () {
+            return scope.wizard.getCurrentStep();
+          },
+          function (step) {
+            if(step){
+              activeExpertId = step.stepGroup;
+            }
+          }
+        );
+
       }
     };
   });
