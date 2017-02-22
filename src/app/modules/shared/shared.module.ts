d21afa9180ec6fe43c3977320bd63ab6d4395f43
zarlex <alex.zarges@online.de>
2017-02-22 13:46:23 +0100

    remove old import

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FullScreenViewComponent} from './components/full-screen-view/full-screen-view.component';
 import {FullScreenViewDirective} from './directives/full-screen-view.directive';
 
 @NgModule({
