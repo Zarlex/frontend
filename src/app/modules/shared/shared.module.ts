ab09d0d8b8411547b22898facb7f542e2c769bdf
zarlex <alex.zarges@online.de>
2017-02-23 00:37:47 +0100

    declare added directives and components

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,16 +1,25 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {ScrollNavigatorDirective} from './directives/scroll-navigator.directive';
 import {FullScreenViewDirective} from './directives/full-screen-view.directive';
+import {SlideManagerComponent} from './components/slide-manager/slide-manager.component';
+import {SlideComponent} from './components/slide/slide.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   exports: [
-    FullScreenViewDirective
+    FullScreenViewDirective,
+    ScrollNavigatorDirective,
+    SlideManagerComponent,
+    SlideComponent
   ],
   declarations: [
-    FullScreenViewDirective
+    FullScreenViewDirective,
+    ScrollNavigatorDirective,
+    SlideManagerComponent,
+    SlideComponent
   ]
 })
 
